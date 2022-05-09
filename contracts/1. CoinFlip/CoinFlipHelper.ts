a8588a7db950f60de4208fa7d3ee4833489c3fad
bluecco <bluecco@gmail.com>
2022-05-09 21:15:11 +0200

    level 1

--- contracts/1. CoinFlip/CoinFlipHelper.ts
+++ contracts/1. CoinFlip/CoinFlipHelper.ts
@@ -3,6 +3,11 @@ import { ethers } from "hardhat";
 
 const helper = async (victim: any, attacker: any) => {
   // add code here that will help you pass the test
+  let consecutiveWins = await victim.consecutiveWins();
+  while (consecutiveWins < 10) {
+    await attacker.hackContract();
+    consecutiveWins = await victim.consecutiveWins();
+  }
 };
 
 export default helper;
